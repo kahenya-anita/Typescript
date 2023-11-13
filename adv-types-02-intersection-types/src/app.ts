7f621a5f41daddd133c69b9c78612e8fb15b0e0b
anita-that-chiq <62019551+kahenya-anita@users.noreply.github.com>
2023-11-13 20:16:52 +0300

    Objects & union types--Discriminating

--- adv-types-02-intersection-types/src/app.ts
+++ adv-types-02-intersection-types/src/app.ts
@@ -44,3 +44,29 @@ function printEmployeeInformation(emp: UnknownEmployee){
   }
 }
 printEmployeeInformation(e1)
+
+interface Bird{
+  type: 'bird'
+  flyingSpeed: number
+}
+
+interface Horse{
+  type: 'horse'
+  runningSpeed: number
+}
+
+type Animal = Bird | Horse
+
+function moveAnimal(animal: Animal){
+  let speed;
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed
+      break;
+    case 'horse':
+      speed = animal.runningSpeed
+      
+  }
+  console.log('Moving at Speed: ' + speed)
+}
+moveAnimal({type: 'bird', flyingSpeed: 10})
