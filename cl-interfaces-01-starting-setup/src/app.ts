9797ab265108692463b82be4f898f834c469a8bf
anita-that-chiq <62019551+kahenya-anita@users.noreply.github.com>
2023-11-13 19:35:35 +0300

    Interfaces

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,5 +1,11 @@
+//Interface inheritance then extend it
+interface Named {
+    readonly gender: string
+    outPutName?: string //optional property
+}
+
 //An interface is used to define structure of an Object
-interface Person{
+interface Person extends Named{ //extending interface
     name: string
     age: number
     
@@ -11,8 +17,19 @@ let user1: Person
 user1 = {
     name: "Max",
     age: 20,
+    gender: "male",
     greet(phrase: string){
         console.log(phrase + ' ' + this.name)
     }
 }
 user1.greet('Hi there I am')
+
+//Interfaces are used to share functionality between classes
+//Interface does not have no implimentation details at all
+//Has readonly property
+interface Person1{
+    readonly name: string
+    age: number
+    
+    greet(phrase: string): void
+}
