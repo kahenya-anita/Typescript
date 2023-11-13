9578af5a06e82597f07995da5ae4e8b3df16f774
anita-that-chiq <62019551+kahenya-anita@users.noreply.github.com>
2023-11-13 20:07:06 +0300

    Type guard

--- adv-types-02-intersection-types/src/app.ts
+++ adv-types-02-intersection-types/src/app.ts
@@ -22,3 +22,25 @@ type Combinable = string | number; //Union type
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric; //Intersection type which takes in any types
+
+function add(a: Combinable, b: Combinable){
+  if(typeof a === 'string' || typeof b === 'string'){ //Type guard
+    return a.toString() + b.toString()
+  }
+  return a + b
+}
+console.log(add("5", "6"))
+
+type UnknownEmployee = Employee | Admin
+
+function printEmployeeInformation(emp: UnknownEmployee){
+  console.log('Name: ' + emp.name)
+  if ('privileges' in emp) {
+    console.log('Privileges: ' + emp.privileges)
+  }
+  if ('startDate' in emp) {
+    console.log('StartDate: ' + emp.startDate)
+    
+  }
+}
+printEmployeeInformation(e1)
